e5fc3244d62a266490748afffe70eb3bbfbd736e
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-17 12:23:42 -0500

    Configuracion de rutas. Adicion de proyecto videos-api.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = () => <h1>¡Hola Mundo!</h1>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* BrowserRouter: maneja las rutas en el navegador. */}
+      <BrowserRouter>
+        {/* Routes: permite definir grupos de rutas. */}
+        <Routes>
+          {/* Route: permite definir la ruta y el componente que se mostrara en esa ruta. */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
