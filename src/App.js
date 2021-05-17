be218a42047e1d3e1bd20c9af407898287d2f452
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-17 13:46:31 -0500

    Definicion de rutas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const Home = () => <h1>¡Hola Mundo!</h1>;
+const NotImplemented = () => <h1>Esta página aún no esta lista</h1>;
 
 function App() {
   return (
@@ -10,8 +10,25 @@ function App() {
         {/* Routes: permite definir grupos de rutas. */}
         <Routes>
           {/* Route: permite definir la ruta y el componente que se mostrara en esa ruta. */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<NotImplemented />} />
         </Routes>
+
+        {/* Rutas para el usuario. */}
+        <Routes>
+          <Route path="/users/signin" element={<NotImplemented />} />
+          <Route path="/users/signup" element={<NotImplemented />} />
+
+          <Route path="/users/:id" element={<NotImplemented />} />
+          <Route path="/users/:id/videos" element={<NotImplemented />} />
+        </Routes>
+
+        {/* Rutas para los videos. */}
+        <Routes>
+          <Route path="/videos" element={<NotImplemented />} />
+          <Route path="/videos/add" element={<NotImplemented />} />
+          <Route path="/videos/:id" element={<NotImplemented />} />
+        </Routes>
+
       </BrowserRouter>
     </div>
   );
